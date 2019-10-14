779570275d553dcf44c71a2518cbd4a71a1795b1
mengya <fengming19981221@163.com>
2019-10-14 14:45:32 +0800

    version05
    版本5,prepare for keyboard listening events
    addkeylistener

--- src/cn/sxt/game/Plane.java
+++ src/cn/sxt/game/Plane.java
@@ -1,9 +1,12 @@
 package cn.sxt.game;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Plane extends GameObject{
 
+    // 飞机的方向
+    boolean left, up, right, down;
 
     public void drawSelf(Graphics graphics) {
         graphics.drawImage(img, (int) x, (int) y, null);
@@ -15,6 +18,25 @@ public class Plane extends GameObject{
         this.img = img;
         this.x = x;
         this.y = y;
+    }
+
+    public void addDirection(KeyEvent event) {
+        switch (event.getKeyCode()) {
+            case 37:
+                left = true;
+                break;
+
+            case 38:
+                up = true;
+                break;
+
+            case 39:
+                right = true;
+
+            case 40:
+                down = true;
+                break;
+        }
     }
 
 }
